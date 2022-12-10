80a531714991e78adc0d22388786795c72f0be1b
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-10 08:17:48 +0000

    firebase checking user and creating in firestore db done

--- src/routes/signin/signin.component.jsx
+++ src/routes/signin/signin.component.jsx
@@ -1,8 +1,12 @@
-import { signInWithGooglePopup } from "../../utils/firebase";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
+    createUserDocumentFromAuth(response.user);
     console.log(response);
   };
   return (
--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -10,6 +10,9 @@ import {
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// import firestore config
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -33,5 +36,28 @@ provider.setCustomParameters({
 });
 
 export const auth = getAuth();
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  // check for the reference
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  // check for the snapshot in db
+  const userSnapshot = await getDoc(userDocRef);
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (err) {
+      console.error("Error creating user", err);
+    }
+  }
+  return userDocRef;
+};
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
