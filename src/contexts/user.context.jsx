9bebafc48f0f4f25ba68e45ab68e972583ec303d
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-13 22:48:57 +0400

    user context state update to use reducer rather than useState

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -8,8 +8,37 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTIONS_TYPE = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  
+  switch (type) {
+  case USER_ACTIONS_TYPE.SET_CURRENT_USER:
+    return {
+      ...state,
+      currentUser: payload
+    };
+
+  default:
+    throw new Error(`Invalid action ${action} in UserReducer`);
+  }
+  
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTIONS_TYPE.SET_CURRENT_USER, payload: user});
+  };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
